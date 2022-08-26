f6069911726f66e7948241312b38fd4de47f2cd3
mike-crane <mike.c0321@gmail.com>
2022-08-26 11:37:05 -0400

    initial commit

---
+++ ui.frontend/src/components/Header/Header.js
@@ -0,0 +1,19 @@
+//Header.js
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+require('./Header.css');
+
+export default class Header extends Component {
+
+    render() {
+        return (
+                <header className="Header">
+                    <div className="Header-container">
+                        <Link to="/content/aem-spa-demo/us/en/home.html">
+                            <h1>WKND</h1>
+                        </Link>
+                    </div>
+                </header>
+        );
+    }
+}
